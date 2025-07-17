a3139cfa9b819032be070a81604e622f87707a2f
Carlos <cps172779@gmail.com>
2025-07-17 14:10:41 -0600

    changes to cloudfront

--- infra/main.tf
+++ infra/main.tf
@@ -36,6 +36,82 @@ resource "aws_s3_bucket" "react_app_bucket" {
   }
 }
 
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Origin Access Identity for React App S3 Bucket"
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  count  = var.create_bucket ? 1 : 0
+
+  bucket = aws_s3_bucket.react_app_bucket[0].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        AWS = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
+      }
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.react_app_bucket[0].arn}/*"
+    }]
+  })
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "CloudFront Distribution for React App"
+
+  origin {
+    domain_name = aws_s3_bucket.react_app_bucket[0].bucket_regional_domain_name
+    origin_id   = "S3-react-app"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-react-app"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_100" # Usa solo regiones económicas (US, Europa)
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
 output "bucket_name" {
   value = var.create_bucket ? aws_s3_bucket.react_app_bucket[0].bucket : var.s3_bucket_name
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.cdn.id
 }
